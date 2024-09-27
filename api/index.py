6c9692476c05be648b17d8b451a4ed60153935ed
ahmedawadio <ahmed@ahmedawad.io>
2024-09-26 23:16:48 -0400

    updating backend

--- api/index.py
+++ api/index.py
@@ -1,16 +1,16 @@
 from flask import Flask, jsonify
-from database import get_location
+# from database import get_location
 
 app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "Hello, World!"
 
 
 
-@app.route("/api/location")
-def location():
-    location_data = get_location()
-    return jsonify(location_data)
+# @app.route("/api/location")
+# def location():
+#     location_data = get_location()
+#     return jsonify(location_data)
 
