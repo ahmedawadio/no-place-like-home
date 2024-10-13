e4d78cf75fe5cadb93b4333c9b79b2e48b4801ec
ahmedawadio <ahmed@ahmedawad.io>
2024-10-13 13:09:20 -0400

    updating config removign vercel.json

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
+            : '/api/index.py', // In production, direct to main API handler
       
             
       },
