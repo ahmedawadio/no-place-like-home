f43929ee9256b20e9ccc38c5d5a69ca3a2f7616d
ahmedawadio <ahmed@ahmedawad.io>
2024-10-13 11:11:28 -0400

    adding tailwind indicators

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,21 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export function TailwindIndicator() {
+  if (process.env.NODE_ENV === 'production') return null
+
+  return (
+    <div className="fixed bottom-1 left-1 z-50 flex size-6 items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white">
+      <div className="block sm:hidden">xs</div>
+      <div className="hidden sm:block md:hidden">sm</div>
+      <div className="hidden md:block lg:hidden">md</div>
+      <div className="hidden lg:block xl:hidden">lg</div>
+      <div className="hidden xl:block 2xl:hidden">xl</div>
+      <div className="hidden 2xl:block">2xl</div>
+    </div>
+  )
+}
+
 export const metadata = { 
   title:  'Nest ML',
   description: 'There is no place like home...kinda',
@@ -21,7 +36,7 @@ export default function RootLayout({
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        
+        <TailwindIndicator />
       <body className={inter.className}>{children}</body>
     </html>
   )
