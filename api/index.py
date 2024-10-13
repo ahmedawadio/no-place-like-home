da3b50132ff2253368a80587ab27dafae130c31e
ahmedawadio <ahmed@ahmedawad.io>
2024-10-13 13:19:31 -0400

    running 1 api end point as a test

--- api/index.py
+++ api/index.py
@@ -14,18 +14,18 @@ def hello_world():
 
 
 
-@app.route("/api/zipcode/<string:zipcode>", methods=["GET"])
-def location(zipcode):
-    if len(zipcode) != 5 or not zipcode.isdigit():
-        return jsonify({"error": "Must be a 5-digit zip code."}), 400
+# @app.route("/api/zipcode/<string:zipcode>", methods=["GET"])
+# def location(zipcode):
+#     if len(zipcode) != 5 or not zipcode.isdigit():
+#         return jsonify({"error": "Must be a 5-digit zip code."}), 400
 
-    response = jsonify(get_location(zipcode))
-    return response
+#     response = jsonify(get_location(zipcode))
+#     return response
 
 
 
-@app.route("/api/image/<string:mid>", methods=["GET"])
-def image(mid):
-    return get_image(mid)
+# @app.route("/api/image/<string:mid>", methods=["GET"])
+# def image(mid):
+#     return get_image(mid)
 
 
