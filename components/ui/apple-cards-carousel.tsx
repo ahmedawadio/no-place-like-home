67fd4ecda5db4a55c55b283de7f00560b2a8c6dd
ahmedawadio <ahmed@ahmedawad.io>
2024-10-15 10:04:57 -0400

    adding pr because cut off on mobile

--- components/ui/apple-cards-carousel.tsx
+++ components/ui/apple-cards-carousel.tsx
@@ -112,8 +112,8 @@ export const Carousel = ({ items, initialScroll = 0 }: CarouselProps) => {
 
           <div
             className={cn(
-              "flex flex-row justify-start gap-4 pl-0",
-              "max-w-7xl mx-auto" // remove max-w-4xl if you want the carousel to span the full width of its container
+              "flex flex-row justify-start gap-4 pl-0 pr-40",
+              "max-w-7xl mx-auto" // remove max-w-4xl if yu want the carousel to span the full width of its container
             )}
           >
             {items.map((item, index) => (
