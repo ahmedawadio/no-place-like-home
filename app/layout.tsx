8b6bd8b65fd051a4b860ae9efecc276aa31fad26
ahmedawadio <ahmed@ahmedawad.io>
2024-11-16 15:55:41 -0500

    updating format

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,50 @@
+import "./globals.css";
+import { Pacifico, Inter } from "next/font/google";
+import TailwindIndicator from "@/components/TailwindIndicator";
 
-import './globals.css'
-import { Pacifico, Inter } from 'next/font/google';
-import TailwindIndicator from '@/components/TailwindIndicator'
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
-
-
-
-export const metadata = { 
-  title:  'Nest ML',
-  description: 'There is no place like home...kinda',
-}
+export const metadata = {
+  title: "Nest ML",
+  description: "There is no place like home...kinda",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-    <head>
-      <title>{metadata.title}</title>
-      <meta name="description" content={metadata.description} />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link rel="icon" href="/favicon.ico" />
-    </head>
-    <body className={inter.className}>
-      <TailwindIndicator />
-      {children}
-    </body>
-  </html>
-  )
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body className={inter.className}>
+        <TailwindIndicator />
+        {children}
+      </body>
+    </html>
+  );
 }
